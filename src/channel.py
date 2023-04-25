894930835245b5fed1734e211dd87a36b74d6831
Karankevich <liza_minyaeva@mail.ru>
2023-04-25 20:18:48 +0300

    добавление метода str, реализация возможности складывать, вычитать, сравнивать два канала

--- src/channel.py
+++ src/channel.py
@@ -19,9 +19,24 @@ class Channel:
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + self.channel_id
-        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = channel['items'][0]['statistics']['videoCount']
-        self.view_count = channel['items'][0]['statistics']['viewCount']
+        self.subscriber_count = int(channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(channel['items'][0]['statistics']['viewCount'])
+
+    def __str__(self):
+        return f'{self.title} ({self.url}'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
 
     @property
     def channel_id(self):
@@ -37,8 +52,7 @@ class Channel:
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
     def to_json(self, file):
-        with open(file, 'w+') as f:
-            data = [self.channel_id, self.title, self.description, self.url, self.subscriber_count, self.video_count, self.view_count]
+        with open(file, 'w+', encoding='utf-8') as f:
+            data = [self.channel_id, self.title, self.description, self.url, self.subscriber_count, self.video_count,
+                    self.view_count]
             json.dump(data, f, ensure_ascii=False)
-
-
