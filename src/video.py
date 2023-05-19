eb8e82ab4283e63b0e984fe2e1e16d4d04b970da
Karankevich <liza_minyaeva@mail.ru>
2023-05-19 20:32:27 +0300

    проверка id видео

--- src/video.py
+++ src/video.py
@@ -4,16 +4,22 @@ from src.channel import youtube
 class Video:
     def __init__(self, video_id):
         self.video_id = video_id
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.name_video = video_response['items'][0]['snippet']['title']
-        self.url_video = 'https://youtu.be/' + video_id
-        self.view_qty = video_response['items'][0]['statistics']['viewCount']
-        self.like_qty = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url_video = 'https://youtu.be/' + video_id
+            self.view_qty = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url_video = None
+            self.view_qty = None
+            self.like_count = None
 
     def __str__(self):
-        return self.name_video
+        return self.title
 
 
 class PLVideo(Video):
@@ -22,4 +28,4 @@ class PLVideo(Video):
         self.pl_id = pl_id
 
     def __str__(self):
-        return self.name_video
+        return self.title
